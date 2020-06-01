852ea1495756fb74d97c9da4776fd0f53d5432a0
SolutionMG <jieun1898@naver.com>
2020-06-01 19:40:34 +0900

    0601_북마크,이메일버튼 추가, 정보 창 갱신 버튼 추가

--- Term/Kuide.py
+++ Term/Kuide.py
@@ -2,55 +2,74 @@ from tkinter import *
 from tkinter import ttk #combobox 용
 
 class MainGUI:
+    def SendEmail(self): #이메일 전송
+        pass
+    
+    def CheckBookmark(self): #북마크 버튼눌렀을 때/
+        pass
+
     def Bookmark(self): #북마크 띄우기
         self.bookmark['state'] = DISABLED
         self.information['state'] = NORMAL
-        # canvas 초기화하고 book리스트 보여주기
+        #canvas 초기화하고 book리스트보여주기
 
     def Information(self): # 정보 띄우기
         self.bookmark['state'] = NORMAL
         self.information['state'] = DISABLED
-        # canvas 초기화하고 Info 리스트 보여주기
+        # canvas 초기화하고 Info리스트보여주기
 
-    def InitInsert(self): # 지역들 xml로부터 읽어와 combobox에 넣어주기 self.do, self.city
+    def InitInsert(self): #지역들 xml로부터 읽어와 combobox에 넣어주기 self.do, self.city
         pass
 
-    def Search(self): # combobox로부터 선택된 값을 얻어와 해당 지역의 관광지를 listbox로 뽑음
+    def Refresh(self): # 관광지 리스트박스에서 선택 후 누르면 정보 창의 정보 갱신
+        pass
+
+    def Search(self): #combobox로부터 선택된 값을 얻어와 해당 지역의 관광지를 listbox로 뽑음
+        #리스트 박스에 해당 지역의 유명 관광지 xml로부터 이름 불러와 insert
         pass
 
     def __init__(self):
         self.window = Tk()
-        self.window.iconbitmap("Kuide.ico")
+        self.window.iconbitmap("Resource/Kuide.ico")
         self.window.title("Kuide")
         self.window.geometry('1280x720')
-        self.window.configure(bg="light pink")
+        self.window.configure(bg= "light pink")
 
-        Label(self.window, text="도", bg="light pink").place(x=20, y= 50)  # 충청남도 할때 도
-        Label(self.window, text="시/군/구", bg="light pink").place(x=130, y=50)  # 시,군,구
-        self.do = ttk.Combobox(self.window, width=8)  # 도 선택 버튼
+        Label(self.window, text = "도",bg= "light pink").place(x = 20, y = 50) #충청남도 할때 도
+        Label(self.window, text = "시/군/구",bg= "light pink").place(x= 130, y = 50) #시,군,구
+        self.do = ttk.Combobox(self.window, width = 8) #도 선택 버튼
         self.do.place(x = 20, y = 80)
-        self.city = ttk.Combobox(self.window, width=10)  # 시/군/구 선택버튼
+        self.city = ttk.Combobox(self.window, width = 10) # 시/군/구 선택버튼
         self.city.place(x = 130, y = 80)
         self.InitInsert() # 선택창에 지역 넣어주기
 
-        Button(self.window,text = "검색",bg = "pale violet red", command = self.Search).place(x = 410, y = 77)
+        Button(self.window,text = "검색",bg = "pale violet red", command = self.Search).place(x = 360, y = 77) #지역 검색 버튼
+        Button(self.window,text = "갱신", bg ="pale violet red", command = self.Refresh).place(x = 400, y= 77) #관광지 선택시 관광지 정보 갱신
 
-        self.TouristDestination = Listbox(self.window, selectmode = 'multiple', width = 60, height= 30)
+        self.TouristDestination = Listbox(self.window, selectmode = 'multiple', width = 60, height= 30) #관광지 리스트
         self.TouristDestination.place(x= 20, y = 120)
-        self.information = Button(self.window, text = "정보", bg = "pink", width = 8, command = self.Information)
-        self.information.place(x =  500, y =95)
-        self.bookmark = Button(self.window, text = "북마크", bg = "pink", width = 8, command = self.Bookmark)
-        self.bookmark.place(x = 568, y=95)
+        self.Information = Button(self.window, text = "정보", bg = "pink", width = 8, command = self.Information) #정보 버튼
+        self.Information.place(x =  500, y =95)
+        self.Bookmark = Button(self.window, text = "북마크", bg = "pink", width = 8, command = self.Bookmark) #북마크 버튼
+        self.Bookmark.place(x = 568, y=95)
 
-        self.InfoandbookmarkList = Listbox(self.window, selectmode = 'multiple', width = 60, height = 15)
+        self.InfoandbookmarkList = Listbox(self.window, selectmode = 'multiple', width = 60, height = 15)#정보,북마크버튼 및의 정보 띄워주는 창
         self.InfoandbookmarkList.place(x = 500, y = 120)
 
-        self.picture = Canvas(self.window, width = 420, height = 210) #관광지 이미지
-        self.picture.place(x = 500, y =  390)
+        self.Picture = Canvas(self.window, width = 420, height = 210) #관광지 이미지
+        self.Picture.place(x = 500, y =  390)
 
         Label(self.window, text= "지도",bg= "light pink").place(x =  950, y =95)
-        self.map = Canvas(self.window, width = 300, height = 450) #지도
-        self.map.place(x = 950, y = 120)
+        self.Map = Canvas(self.window, width = 300, height = 450) #지도
+        self.Map.place(x = 950, y = 120)
+
+        #0601추가_북마크버튼
+        self.PhotoBookmark = PhotoImage(file='Resource/BookmarkButton.png')
+        Button(self.window, image = self.PhotoBookmark, command = self.CheckBookmark, bg = "pink").place(x = 840, y=85)
+
+        #0601추가_이메일전송버튼
+        self.PhotoEmail = PhotoImage(file='Resource/Email.png')
+        Button(self.window, image = self.PhotoEmail, command = self.SendEmail, bg = "pink").place(x = 880, y = 85)
 
         self.window.mainloop()
 
