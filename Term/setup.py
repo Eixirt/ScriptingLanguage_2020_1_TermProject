a3ba56ce8b364d8361bf854a9c137d9d1ae49d56
SolutionMG <jieun1898@naver.com>
2020-06-20 00:10:38 +0900

    20200620_최종배포_setup파일

---
+++ Term/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup, Extension
+setup(name='Kuide',
+version='1.0',
+py_modules=['Gmail', 'mysmtplib', 'OpenApiSigungu','OpenApiParsing','OpenApiDo','DrawMap','Kuide','TourInformation'],
+data_files=['Resource\BasicImage.png','Resource\BookmarkButton.png','Resource\Email.png','Resource\Kuide.ico','Resource\Map.png'])
